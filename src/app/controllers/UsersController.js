b195bd8762ab31f6995a5d271886c223cfdcb022
Andre Teles <andre.telestp@gmail.com>
2020-07-01 10:23:24 -0300

    finished resource users

--- src/app/controllers/UsersController.js
+++ src/app/controllers/UsersController.js
@@ -1,32 +1,18 @@
+import ResourceController from './Controller';
 import User from '../models/User';
 import validator from '../validation/validator';
 import UserValidation from '../validation/users';
 
-class UsersController {
-  async index(req, res) {
-    const users = await User.findAll();
-    res.json(users);
+class UsersController extends ResourceController {
+  constructor() {
+    super(User);
   }
 
-  async find(req, res) {
-    try {
-      const users = await User.findOne(req.params.id);
-      res.json(users);
-    } catch (error) {}
-
-    return res.status(400).json({});
-  }
-
-  update(req, res) {}
-
-  async create(req, res) {
-    await validator.validate(req.body, UserValidation.rules());
-
-    if (validator.fails()) {
-      return res.status(400).json({ errors: validator.getErrors() });
-    }
-    const user = await User.create(req.body);
-    return res.json(user);
+  runValidator() {
+    return {
+      validator,
+      EntityValidation: UserValidation,
+    };
   }
 }
 
