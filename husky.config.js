001c1d860d0919c00a1daa4f8b9cf7d0bb68e4a0
Andre Teles <andre.telestp@gmail.com>
2020-07-23 20:57:29 -0300

    removed entry pre-merge-commit

--- husky.config.js
+++ husky.config.js
@@ -4,8 +4,7 @@ const tasks = (arr) => arr.join(' && ');
 module.exports = {
   hooks: {
     interactive: true,
-    // 'pre-commit': tasks(['yarn lint', 'yarn test']),
+    'pre-commit': tasks(['yarn lint', 'yarn test']),
     'pre-push': tasks(['yarn lint', 'yarn test']),
-    'pre-merge-commit': tasks(['yarn lint', 'yarn test']),
   },
 };
